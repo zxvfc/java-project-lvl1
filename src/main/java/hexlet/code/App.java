48948b30de8cc4e81a02897e77e2bcf5b5e47545
velya <velya.96@gmail.com>
2021-03-12 16:23:52 +0300

    Added interactive menu

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,9 +1,18 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(final String[] args) {
         System.out.println("Welcome to the Brain Games!");
-        Cli.greetings();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n0 - Exit");
+        System.out.print("Your choice: ");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1 -> Cli.greetings();
+        };
     }
 }
