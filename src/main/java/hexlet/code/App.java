aea5db3914ed00ba2fc09d09cafd8b4b3d9e6b05
velya <velya.96@gmail.com>
2021-03-15 01:44:58 +0300

    Added Progression game

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -3,19 +3,29 @@ package hexlet.code;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
 import hexlet.code.games.Gcd;
+import hexlet.code.games.Progression;
 import java.util.Scanner;
 
 public class App {
+
+    private static final String MENU = """
+            1 - Greet
+            2 - Even
+            3 - Calc
+            4 - GCD
+            5 - Progression
+            0 - Exit""";
 
     private static final int GREETINGS = 1;
     private static final int EVEN = 2;
     private static final int CALC = 3;
     private static final int GCD = 4;
+    private static final int PROGRESSION = 5;
 
     public static void main(final String[] args) {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n0 - Exit");
+        System.out.println(MENU);
         System.out.print("Your choice: ");
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
@@ -25,6 +35,7 @@ public class App {
             case EVEN -> Engine.runGame(new Even());
             case CALC -> Engine.runGame(new Calc());
             case GCD -> Engine.runGame(new Gcd());
+            case PROGRESSION -> Engine.runGame(new Progression());
             default -> {
             }
         }
---
+++ src/main/java/hexlet/code/games/Progression.java
@@ -0,0 +1,46 @@
+package hexlet.code.games;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class Progression extends Game {
+
+    private static final int MIN_STEP = 1;
+    private static final int MAX_STEP = 5;
+
+    private static final int MIN_LENGTH = 5;
+    private static final int MAX_LENGTH = 10;
+
+    private static final String HIDE_SYMBOL = "..";
+    private static final String DELIMITER = " ";
+
+    @Override
+    public String getDescription() {
+        return "What number is missing in the progression?";
+    }
+
+    @Override
+    public GameData generateData() {
+        final List<String> sequence = generateSequence();
+
+        final int hiddenPosition = getRandomNumber() % sequence.size();
+
+        final String answer = sequence.get(hiddenPosition);
+        sequence.set(hiddenPosition, HIDE_SYMBOL);
+        final String question = String.join(DELIMITER, sequence);
+
+        return new GameData(question, answer);
+    }
+
+    private List<String> generateSequence() {
+        final int baseNumber = getRandomNumber();
+        final int step = getRandomNumber() % MAX_STEP + MIN_STEP;
+        final int length = getRandomNumber() % MAX_LENGTH + MIN_LENGTH;
+
+        return IntStream.iterate(baseNumber, n -> n + step)
+                        .limit(length)
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.toList());
+    }
+}
